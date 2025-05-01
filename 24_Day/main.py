9532b94f67dd5f87946131241744b49718685b77
Gouresh33 <166603693+Gouresh33@users.noreply.github.com>
2025-05-01 15:14:18 +0530

    open, write, read to files using the keyword "with"

---
+++ 24_Day/main.py
@@ -0,0 +1,9 @@
+with open("my_file.txt") as file:
+    content = file.read()
+    print(content)
+
+with open("my_file.txt", mode="a") as file:
+    file.write("\nNew text.")
+
+with open("new_file.txt", mode="w") as file:
+    file.write("\nNew text.")
