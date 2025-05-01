30282c3205b9cc0c639219895fbabf0d3e2a02af
Gouresh33 <166603693+Gouresh33@users.noreply.github.com>
2025-05-01 14:56:55 +0530

    improved the snake game to keep high score

--- 20&21_Day/snake_game/main.py
+++ 20&21_Day/snake_game/main.py
@@ -35,12 +35,12 @@ while game_is_on:
         score.incre_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.Game_over()
+        score.reset()
+        snake.reset()
 
     for segments in snake.segments[1:]:
         if snake.head.distance(segments) < 10:
-            game_is_on = False
-            score.Game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- 20&21_Day/snake_game/scoreboard.py
+++ 20&21_Day/snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class scoreboared(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +16,19 @@ class scoreboared(Turtle):
         self.hideturtle()
 
     def Updatescore(self):
-        self.write(arg=f"Score:{self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(arg=f"Score:{self.score} Highscore:{self.high_score}", align=ALIGN, font=FONT)
 
-    def Game_over(self):
-        self.goto(0,0)
-        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.Updatescore()
+
+    # def Game_over(self):
+    #     self.goto(0,0)
+    #     self.write(arg="GAME OVER", align=ALIGN, font=FONT)
 
     def incre_score(self):
         self.score += 1
-        self.clear()
         self.Updatescore()
